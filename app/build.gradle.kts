0fffbf666bfbff85ba18c9c8db00a14475e6d888
narvik6 <128902127+narvik6@users.noreply.github.com>
2026-05-15 00:09:32 +0300

    1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.material.icons.core)
 
     // Архитектура, Навигация и UI
     implementation(libs.androidx.lifecycle.viewmodel.compose)
